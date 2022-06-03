c3b650e6c8ed11a386a7543ea3d636c0a4e2001d
nguyenhaison <nguyenhaison7@gmail.com>
2022-06-04 00:55:04 +0700

    API.js

---
+++ src/utils/API.js
@@ -0,0 +1,22 @@
+import {
+  _getUsers,
+  _getQuestions,
+  _saveQuestion,
+  _saveQuestionAnswer,
+} from "./_DATA";
+
+export async function getInitialData() {
+  const [users, questions] = await Promise.all([_getUsers(), _getQuestions()]);
+  return {
+    users,
+    questions,
+  };
+}
+
+export function saveQuestion(info) {
+  return _saveQuestion(info);
+}
+
+export function saveQuestionAnswer(info) {
+  return _saveQuestionAnswer(info);
+}
