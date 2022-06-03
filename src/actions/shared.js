885cf00559cb96d5adbded7f3e81e7bea996e03c
nguyenhaison <nguyenhaison7@gmail.com>
2022-06-04 00:59:43 +0700

    actions

---
+++ src/actions/shared.js
@@ -0,0 +1,11 @@
+import { getInitialData } from "../utils/api";
+import { receiveQuestions } from "../actions/questions";
+import { receiveUsers } from "../actions/users";
+
+export function handleInitialData() {
+  return async (dispatch) => {
+    const { users, questions } = await getInitialData();
+    dispatch(receiveUsers(users));
+    dispatch(receiveQuestions(questions));
+  };
+}
